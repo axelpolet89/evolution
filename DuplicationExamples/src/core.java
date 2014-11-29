e6f1c96f12e3c18311df06607e96fc565da906fc
axelpolet89 <axel.polet33@gmail.com>
2014-11-29 22:53:28 +0100

    + testing harnass (initial)

---
+++ DuplicationExamples/src/core.java
@@ -0,0 +1,54 @@
+public class core {
+	
+	private int initial = 1;
+	
+	/**
+	 * JavaDoc
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
+	
+	/*
+	 * DupSource, DupSource regel 1-15
+	 * DupSource, DupSource, SmallInnerDuplicate, SmallOuterDuplicate regel 2-8
+	 * DupSource, DupSource, FullInnerDuplicate, regel 2-15
+	 * DupSource, DupSource, SmallOuterDupicate2, regel 7-15
+	 */
+	
+	//comment
+	public void DupSource()
+	{
+		String test = "";
+		String test1 = "1";
+		String test2 = "2";
+		String test3 = "3";
+		if(test2 == "")
+		{
+			test3 = "1+1";
+		}
+		for(int i = 0; i < 10; i++)
+		{
+			test = "lala";
+		}
+	}
+	
+	private int InBetween()
+	{
+		String test = "this is inbetween";
+		return 1;
+	}
+	
+	public void SmalInnerDuplicate()
+	{
+		String s11 = "s11";
+		String test1 = "1";
+		String test2 = "2";
+		String test3 = "3";
+		if(test2 == "")
+		{
+			test3 = "1+1";
+		}
+		String s12 = "s12";
+	}
+}
---
+++ DuplicationExamples/src/utils.java
@@ -0,0 +1,68 @@
+public class utils
+{	
+	//same name
+	public void DupSource()
+	{
+		String test = "";
+		String test1 = "1";
+		String test2 = "2";
+		String test3 = "3";
+		if(test2 == "")
+		{
+			test3 = "1+1";
+		}
+		for(int i = 0; i < 10; i++)
+		{
+			test = "lala";
+		}
+	}
+	
+	private void HelloWorld()
+	{
+		String test = "helloworld";
+	}
+	
+	public void SmallOuterDuplicate()
+	{
+		String s1 = "s1";
+		String test1 = "1";
+		String test2 = "2";
+		String test3 = "3";
+		if(test2 == "")
+		{
+			test3 = "1+1";
+		}
+		String e1 = "s2";
+	}
+	
+	/*
+	public void SmallOuterDuplicate2()
+	{
+		String anders = "anders";
+		String test2 = "2";
+		String test3 = "3";
+		String test = "";
+		String test1 = "1";
+		if(test2 == "")
+		{
+			test3 = "1+1";
+		}
+		for(int i = 0; i < 10; i++)
+		{
+			test = "lala";
+		}
+	}
+	*/
+	
+	public void NoDuplicate()
+	{
+		String test = "";
+		String test1 = "1";
+		String test2 = "2";
+		String test4 = "4";
+		if(test == "")
+		{
+			test1 = "1+1";
+		}
+	}
+}
